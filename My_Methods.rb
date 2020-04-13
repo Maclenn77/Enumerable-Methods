2c862ecf718274ced893ec588a8980167e191ea1
Maclenn77 <maclenn77@gmail.com>
2020-04-13 12:01:48 -0500

    Created My_Each Method and tested

--- My_Methods.rb
+++ My_Methods.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/ruby
-
 module Enumerable
 
-def my_Each
-  for i in self.my_Each
-    i
+  def my_Each
+    i = 0
+    loop do
+      self[i]
+      i += 1
+      yield(i)
+      break if self[i].nil?
+    end
   end
 end
+
+[1,3,4,5,6].my_Each do |i| puts i end
