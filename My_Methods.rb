62c2a473c19bdeee1ae04deadf0a5013687acaea
Maclenn77 <maclenn77@gmail.com>
2020-04-13 12:26:36 -0500

    Fixed my_each

--- My_Methods.rb
+++ My_Methods.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/ruby
 module Enumerable
 
-  def my_Each
+  def my_each
     i = 0
     loop do
       self[i]
+      yield(self[i])
       i += 1
-      yield(i)
       break if self[i].nil?
     end
   end
 end
 
-[1,3,4,5,6].my_Each do |i| puts i end
+[1,3,4,5,6].my_each do |i| puts i end
+
+[1,3,4,5,6].each do |i| puts i end
